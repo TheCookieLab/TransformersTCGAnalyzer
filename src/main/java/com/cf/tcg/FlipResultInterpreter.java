235f43c551a3083302bb1d34d2df3816fb14dd60
David Pang <david.pang@veeva.com>
2019-07-18 10:39:27 -0400

    can interpret aggregate flip results

--- src/main/java/com/cf/tcg/FlipResultInterpreter.java
+++ src/main/java/com/cf/tcg/FlipResultInterpreter.java
@@ -1,11 +1,51 @@
 package com.cf.tcg;
 
+import com.cf.tcg.model.Pip;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class FlipResultInterpreter {
 
-    public final FlipResult flipResult;
+    public final List<FlipResult> flipResults;
 
-    public FlipResultInterpreter(FlipResult flipResult) {
-        this.flipResult = flipResult;
+    private final DescriptiveStatistics whitePipStats;
+    private final DescriptiveStatistics orangePipStats;
+    private final DescriptiveStatistics bluePipStats;
+    private final DescriptiveStatistics blackPipStats;
+    private final DescriptiveStatistics greenPipStats;
+
+    public FlipResultInterpreter(List<FlipResult> flipResults) {
+        this.flipResults = flipResults;
+        this.whitePipStats = new DescriptiveStatistics();
+        this.orangePipStats = new DescriptiveStatistics();
+        this.bluePipStats = new DescriptiveStatistics();
+        this.blackPipStats = new DescriptiveStatistics();
+        this.greenPipStats = new DescriptiveStatistics();
+
+        for (FlipResult flipResult : this.flipResults) {
+            this.whitePipStats.addValue(flipResult.getTotalNumberOfPipsFlipped(Pip.WHITE).doubleValue());
+            this.orangePipStats.addValue(flipResult.getTotalNumberOfPipsFlipped(Pip.ORANGE).doubleValue());
+            this.bluePipStats.addValue(flipResult.getTotalNumberOfPipsFlipped(Pip.BLUE).doubleValue());
+            this.blackPipStats.addValue(flipResult.getTotalNumberOfPipsFlipped(Pip.BLACK).doubleValue());
+            this.greenPipStats.addValue(flipResult.getTotalNumberOfPipsFlipped(Pip.GREEN).doubleValue());
+        }
     }
 
+    public double getAverageDamageBonus() {
+        return this.orangePipStats.getMean();
+    }
+
+    public double getAverageArmorBonus() {
+        return this.bluePipStats.getMean();
+    }
+
+    public double getAveragePierceBonus() {
+        return this.blackPipStats.getMean();
+    }
+
+    public Integer getFlipResultCount() {
+        return this.flipResults.size();
+    }
 }
---
+++ src/test/java/com/cf/tcg/FlipInterpreterTest.java
@@ -0,0 +1,47 @@
+
+package com.cf.tcg;
+
+import com.cf.tcg.model.BattleCard;
+import com.cf.tcg.model.Pip;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author David
+ */
+public class FlipInterpreterTest {
+    
+    public FlipInterpreterTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+     @Test
+     public void hello() {
+         List<FlipResult> flipResults = new ArrayList<>();
+         flipResults.add(new FlipResult(new BattleCard(Pip.ORANGE)));
+         flipResults.add(new FlipResult(new BattleCard(Pip.WHITE), new BattleCard(Pip.BLUE)));
+         flipResults.add(new FlipResult(new BattleCard(Pip.BLACK, Pip.BLUE)));
+         flipResults.add(new FlipResult(new BattleCard(Pip.WHITE), new BattleCard(Pip.GREEN)));
+         flipResults.add(new FlipResult(new BattleCard(Pip.WHITE, Pip.BLUE, Pip.ORANGE)));
+
+         FlipResultInterpreter interpreter = new FlipResultInterpreter(flipResults);
+
+         assertEquals(0.4, interpreter.getAverageDamageBonus(), 0.001);
+         assertEquals(0.6, interpreter.getAverageArmorBonus(), 0.001);
+         assertEquals(0.2, interpreter.getAveragePierceBonus(), 0.001);
+     }
+}
